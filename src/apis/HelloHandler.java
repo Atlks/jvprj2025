4dc574352504a140702362b0f165b9728f05a2a4
domas <domas@domas>
2025-01-21 00:18:16 +0700

    ...

--- src/apis/HelloHandler.java
+++ src/apis/HelloHandler.java
@@ -6,25 +6,22 @@ import com.sun.net.httpserver.HttpHandler;
 import java.io.IOException;
 import java.io.OutputStream;
 
+import static util.util2026.getRequestParameter;
+
 // 自定义的请求处理器
 public class HelloHandler implements HttpHandler {
     @Override
     public void handle(HttpExchange exchange) throws IOException {
         // 检查请求方法
-        if ("GET".equals(exchange.getRequestMethod())) {
+     //   if ("GET".equals(exchange.getRequestMethod())) {
             String response = "Hello, this is a REST response!";
+        String id=    getRequestParameter(exchange,"id");
+        System.out.println(id);
             exchange.getResponseHeaders().set("Content-Type", "text/plain; charset=utf-8");
             exchange.sendResponseHeaders(200, response.getBytes().length);
             OutputStream os = exchange.getResponseBody();
             os.write(response.getBytes());
             os.close();
-        } else {
-            // 处理非GET请求
-            String response = "Method not allowed";
-            exchange.sendResponseHeaders(405, response.getBytes().length);
-            OutputStream os = exchange.getResponseBody();
-            os.write(response.getBytes());
-            os.close();
-        }
+    //    }
     }
 }
---
+++ src/util/util2026.java
@@ -0,0 +1,38 @@
+package util;
+
+import com.sun.net.httpserver.HttpExchange;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.net.URI;
+import java.util.HashMap;
+import java.util.Map;
+public class util2026 {
+
+    public static String getRequestParameter(HttpExchange exchange,String name)
+    {
+        Map<String, String> queryParams = parseQueryParams(exchange.getRequestURI());
+        String id = queryParams.get(name); // 获取 id 参数
+ return  id;
+    }
+
+    // 解析查询参数的方法
+    public static Map<String, String> parseQueryParams(URI uri) {
+        Map<String, String> queryParams = new HashMap<>();
+        String query = uri.getQuery(); // 获取查询字符串（例如 ?id=123）
+        if (query != null) {
+            String[] pairs = query.split("&"); // 按 & 分割多个参数
+            for (String pair : pairs) {
+                String[] keyValue = pair.split("=", 2); // 按 = 分割键值对
+                if (keyValue.length == 2) {
+                    queryParams.put(keyValue[0], keyValue[1]);
+                } else {
+                    queryParams.put(keyValue[0], ""); // 没有值时设为空字符串
+                }
+            }
+        }
+        return queryParams;
+    }
+}
