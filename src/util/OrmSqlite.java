48c8eb794cbd8f7425acd1923775daa1d5f3b503
domas <domas@domas>
2025-01-29 21:41:05 +0700

    orm sqlite

--- src/util/OrmSqlite.java
+++ src/util/OrmSqlite.java
@@ -1,4 +1,31 @@
 package util;
 
-public class OrmSqlite {
+import apiUsr.Usr;
+
+import java.lang.reflect.Field;
+
+public class OrmSqlite extends OrmBase{
+
+    public static void main(String[] args) throws Exception {
+        OrmMysql orm = new OrmMysql();
+        orm.jdbcurl = "jdbc:sqlite:/dbx/usr22.db";
+
+        Usr u = new Usr();
+        u.id = "888";
+        orm.merge(u);
+
+
+
+    }
+
+    @Override
+    public String _toSqlType(Class javatype) {
+        return switch (javatype.getSimpleName()) {
+            case "String" -> "text";
+            case "Integer", "int" -> "integer";
+            case "Long", "long" -> "integer";
+            case "BigDecimal" -> "real";
+            default -> "text";
+        };
+    }
 }
