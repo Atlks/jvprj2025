d8c19defc9df1d6d5196611e22fc564f60587bcc
domas <domas@domas>
2025-02-21 14:44:06 +0700

    name nlp

--- src/exmp/nameBoy4frnr.kt
+++ src/exmp/nameBoy4frnr.kt
@@ -8,7 +8,7 @@ import util.Util2025.encodeJson
 import java.io.File
 
 //得到最好起名的汉字，笔画少，语音简单，含义好
-//大义力达易
+//大力易达义
 fun main() {
       getSmpName4boy()
     val liRzt = mutableListOf<CharBo>() // 正确初始化可变列表
