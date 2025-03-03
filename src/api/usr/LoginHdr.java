4d9f9b88623d14bac6496163d589edfb0b0ce5c5
domas <domas@domas>
2025-03-03 11:12:53 +0700

    opotmz

--- src/api/usr/LoginHdr.java
+++ src/api/usr/LoginHdr.java
@@ -73,8 +73,11 @@ public class LoginHdr implements Icall<Usr,Object>, HttpAuthenticationMechanism,
             //also set cookie todo
             ResponsRet rt=new ResponsRet();
             rt.reqUrl= String.valueOf(httpExchangeCurThrd.get().getRequestURI());
-            rt.ret= Collections.singletonMap("token", JwtUtil.generateToken(Udto.uname));
+            String tokenJwt = JwtUtil.generateToken(Udto.uname);
+            rt.ret= Collections.singletonMap("tokenJwt", tokenJwt);
+            setcookie("tokenJwt", tokenJwt, httpExchangeCurThrd.get());
             return  rt;
+
         }
         return  "";
 
@@ -82,57 +85,57 @@ public class LoginHdr implements Icall<Usr,Object>, HttpAuthenticationMechanism,
 
 
 
-    public Object calllori(@ModelAttribute Usr Udto) throws Exception, PwdErrEx {
-
-        usrdto.set(Udto);
-        // login(Udto);
-        org.hibernate.Session session = sessionFactory.getCurrentSession();
-        //  om.jdbcurl=saveDirUsrs;
-        //todo start tx
-        var pwd = Udto.pwd;
-        String uname = Udto.uname;
-        Usr u = session.find(Usr.class, uname);
-        if (u == null) {  //u not exist
-            UserNotExistEx e = new UserNotExistEx("用户名错误");
-            e.fun = getCurrentMethodName();
-            e.funPrm = new Usr(uname, pwd);
-            throw e;
-        }
-        if (u.pwd.equals(pwd))
-        {
-
-            Passport passport = new Passport();
-
-            passport.setHolderName(uname);
-
-
-
-            VisaService visaService = new VisaService();
-            Visa visa = visaService.applyForVisa(passport, "Thailand", "Tourist");
-            String val = encodeJson(visa);
-            setcookie("visa", val, httpExchangeCurThrd.get());
-          //  setcookie("uname", encryptDESToStrBase64(uname, Key_a1235678), httpExchangeCurThrd.get());
-            return true;
-        }
-
-        else {
-            //pwd not eq
-            Usr dto = new Usr();
-            dto.uname = uname;
-            dto.pwd = pwd;
-
-            PwdErrEx e = new PwdErrEx("密码错误");
-            e.fun = getCurrentMethodName();
-            e.funPrm = new Usr(uname, pwd);
-
-            throw e;
-        }
-
-
-        //   setcookie("uname", Udto.uname, httpExchangeCurThrd.get());
-        //   return Udto;
-
-    }
+//    public Object calllori(@ModelAttribute Usr Udto) throws Exception, PwdErrEx {
+//
+//        usrdto.set(Udto);
+//        // login(Udto);
+//        org.hibernate.Session session = sessionFactory.getCurrentSession();
+//        //  om.jdbcurl=saveDirUsrs;
+//        //todo start tx
+//        var pwd = Udto.pwd;
+//        String uname = Udto.uname;
+//        Usr u = session.find(Usr.class, uname);
+//        if (u == null) {  //u not exist
+//            UserNotExistEx e = new UserNotExistEx("用户名错误");
+//            e.fun = getCurrentMethodName();
+//            e.funPrm = new Usr(uname, pwd);
+//            throw e;
+//        }
+//        if (u.pwd.equals(pwd))
+//        {
+//
+//            Passport passport = new Passport();
+//
+//            passport.setHolderName(uname);
+//
+//
+//
+//            VisaService visaService = new VisaService();
+//            Visa visa = visaService.applyForVisa(passport, "Thailand", "Tourist");
+//            String val = encodeJson(visa);
+//            setcookie("visa", val, httpExchangeCurThrd.get());
+//          //  setcookie("uname", encryptDESToStrBase64(uname, Key_a1235678), httpExchangeCurThrd.get());
+//            return true;
+//        }
+//
+//        else {
+//            //pwd not eq
+//            Usr dto = new Usr();
+//            dto.uname = uname;
+//            dto.pwd = pwd;
+//
+//            PwdErrEx e = new PwdErrEx("密码错误");
+//            e.fun = getCurrentMethodName();
+//            e.funPrm = new Usr(uname, pwd);
+//
+//            throw e;
+//        }
+//
+//
+//        //   setcookie("uname", Udto.uname, httpExchangeCurThrd.get());
+//        //   return Udto;
+//
+//    }
 
 
 //    private String encryMd5(String s, String salt) {
@@ -163,6 +166,7 @@ public class LoginHdr implements Icall<Usr,Object>, HttpAuthenticationMechanism,
             }
             String val = encodeJson(visa);
             setcookie("visa", val, httpExchangeCurThrd.get());
+            setcookie("unameRRZ", dto.uname, httpExchangeCurThrd.get());
             try {
                 setcookie("uname", encryptAesToStrBase64(uname, Key4pwd4aeskey), httpExchangeCurThrd.get());
             } catch (Exception e) {
