38a1b2ff85d8d43766ee9db1d50d3786248e216b
domas <domas@domas>
2025-03-11 14:19:13 +0700

    isam

--- src/service/auth/ISAM.java
+++ src/service/auth/ISAM.java
@@ -8,27 +8,43 @@ import jakarta.security.enterprise.credential.Credential;
 import jakarta.security.enterprise.identitystore.CredentialValidationResult;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import service.secury.*;
+
+import java.util.List;
 
 /**
  * sam安全授权模块   stoer in db
  * 包括俩个部分  keymng 和 安全审计日志接口
  */
-public interface ISAM extends HttpAuthenticationMechanism,IKeyMngr,Alarm, IDPS, SWG, ASB, SASE, DLP {
+public interface ISAM extends HttpAuthenticationMechanism, IKeyMngr, Alarm, IDPS, SWG, ASB, SASE, DLP {
 
     default AuthenticationStatus validateRequest(HttpServletRequest request, HttpServletResponse response, HttpMessageContext httpMessageContext) throws AuthenticationException {
         return AuthenticationStatus.SUCCESS;
     }
 
     /**
-     *    oridat,,,pwd  fgrprint ,faceid
+     * oridat,,,pwd  fgrprint ,faceid
+     *
      * @param oriData
      * @return
      */
-    String geneKey(String oriData);
-    public void storeKey(String uid, String oriKeyData);
+    @NotBlank
+    String geneKey(@NotNull String oriData);
 
-    CredentialValidationResult validate(Credential credential);
-    public void addLogVldFail(String uid, Throwable e);
-    public void addLogVldSucess(String uid);
+    public void storeKey(@NotBlank String uid, @NotBlank String oriKeyData);
+
+    CredentialValidationResult validate(@NotNull Credential credential);
+
+    public void addLogVldFail(@NotBlank String uid, @NotNull Throwable e);
+
+    public void addLogVldSucess(@NotBlank String uid);
+
+    default @NotNull List<Object> listLog() {
+        return List.of();
+    }
+
+    ;
+
 }
