60bef6421c63906c0fc4f422d2ec75366667df37
domas <domas@domas>
2025-03-26 01:12:02 +0700

    optmz lgn

---
+++ doc2503/farm.js
@@ -0,0 +1,107 @@
+function wall(){
+ // 圆的参数
+    const centerX = canvas.width / 2;
+    const centerY = canvas.height / 2;
+    const radius = rds*beishu; // 半径 = 直径 / 2
+    console.log("rds="+radius)
+
+    // 绘制绿色填充的圆
+    ctx.beginPath();
+    ctx.arc(centerX, centerY, radius, 0, Math.PI * 2);
+    ctx.fillStyle = "yellow";
+    ctx.fill();
+
+     // 设置褐色边框
+        ctx.strokeStyle = "brown"; // 颜色
+        ctx.lineWidth = 5; // 线条宽度
+        ctx.stroke(); // 画边框
+
+
+    ctx.closePath();
+
+}
+
+
+function wall2(radius){
+ // 圆的参数
+    const centerX = canvas.width / 2;
+    const centerY = canvas.height / 2;
+  //  const radius = rds*beishu; // 半径 = 直径 / 2
+    console.log("rds="+radius)
+
+    // 绘制绿色填充的圆
+    ctx.beginPath();
+    ctx.arc(centerX, centerY, radius, 0, Math.PI * 2);
+    ctx.fillStyle = "green";
+    ctx.fill();
+
+
+    // 设置褐色边框
+            ctx.strokeStyle = "brown"; // 颜色
+            ctx.lineWidth = 20; // 线条宽度
+            ctx.stroke(); // 画边框
+
+
+    ctx.closePath();
+
+}
+
+
+//紫色区域
+function anml(radius){
+ // 圆的参数
+    const centerX = canvas.width / 2;
+    const centerY = canvas.height / 2;
+  //  const radius = rds*beishu; // 半径 = 直径 / 2
+    console.log("rds="+radius)
+
+    // 绘制 填充的圆
+    ctx.beginPath();
+    ctx.arc(centerX, centerY, radius, 0, Math.PI * 2);
+    ctx.fillStyle = "purple"; // 紫色区域
+    ctx.fill();
+
+
+    // === 添加文本 ===
+        ctx.fillStyle = "black"; // 文字颜色
+        ctx.font = "30px Arial"; // 字体大小和样式
+        ctx.textAlign = "center"; // 文字居中对齐
+        ctx.textBaseline = "middle"; // 文字基线居中
+        ctx.fillText("anmls", centerX-200+100, centerY); // 在圆心绘制文字
+
+
+  // 设置褐色边框
+        ctx.strokeStyle = "brown"; // 颜色
+        ctx.lineWidth = 10; // 线条宽度
+        ctx.stroke(); // 画边框
+
+    ctx.closePath();
+
+}
+
+
+function waterCenter(radius){
+ // 圆的参数
+    const centerX = canvas.width / 2;
+    const centerY = canvas.height / 2;
+  //  const radius = rds*beishu; // 半径 = 直径 / 2
+    console.log("rds="+radius)
+
+    // 绘制绿色填充的圆
+    ctx.beginPath();
+    ctx.arc(centerX, centerY, radius, 0, Math.PI * 2);
+    ctx.fillStyle = "blue";
+    ctx.fill();
+
+
+    // === 添加文本 ===
+        ctx.fillStyle = "black"; // 文字颜色
+        ctx.font = "30px Arial"; // 字体大小和样式
+        ctx.textAlign = "center"; // 文字居中对齐
+        ctx.textBaseline = "middle"; // 文字基线居中
+        ctx.fillText("water", centerX, centerY); // 在圆心绘制文字
+
+
+    ctx.closePath();
+
+}
