6b2918d35951ebc37e8d323be5851e7c35a41f85
domas <domas@domas>
2025-04-13 12:38:44 +0700

    jwt chk

--- src/tools/news.kt
+++ src/tools/news.kt
@@ -5,6 +5,31 @@ import org.jsoup.nodes.Document
 import java.net.HttpURLConnection
 import java.net.URL
 
+
+fun main() {
+
+    var dw = "https://www.dw.com/zh/%E5%9C%A8%E7%BA%BF%E6%8A%A5%E5%AF%BC/s-9058"
+    var Url_fri = "https://www.rfi.fr/cn/"
+    var url_abc="https://www.abc.net.au/news/chinese"
+    showNews(url_abc)
+ //   showNews(dw)
+  //  showNews(Url_fri)
+
+}
+
+private fun showNews(Url_fri: String) {
+    var html = getHtml(Url_fri);
+    var news: List<String> = getListString(html)
+    for (newItem in news) {
+        if (isDontCotainChnsChars(newItem))
+            continue
+        if (newItem.length < 10 || newItem.length>50)
+            continue
+        println(newItem)
+    }
+}
+
+
 /**
  * 解析html，获取所有a标签，读取文本，
  */
@@ -13,22 +38,6 @@ fun getListString(html: String): List<String> {
     return doc.select("a")  // 获取所有 a 标签
         .map { it.text().trim() }  // 读取文本并去除前后空白
         .filter { it.isNotEmpty() }  // 过滤空字符串
-}
-
-fun main() {
-
-    var dw = "https://www.dw.com/zh/%E5%9C%A8%E7%BA%BF%E6%8A%A5%E5%AF%BC/s-9058"
-    var Url_fri = "https://www.rfi.fr/cn/"
-    var html = getHtml(Url_fri);
-    var news: List<String> = getListString(html)
-    for (newItem in news) {
-        if(isDontCotainChnsChars(newItem))
-            continue
-        if(newItem.length<10)
-            continue
-        println(newItem)
-    }
-
 }
 
 /**
