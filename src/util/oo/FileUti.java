fdc4990f3c5dd162aedbd9a35901729e316869f8
dms <dms@uke>
2025-05-09 00:39:33 +0700

    file cache

--- src/util/oo/FileUti.java
+++ src/util/oo/FileUti.java
@@ -1,8 +1,67 @@
 package util.oo;
 
+import util.cache.CantFindFileEx;
+
 import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.attribute.BasicFileAttributes;
+import java.time.Duration;
+import java.time.Instant;
 
 public class FileUti {
+
+
+    /**
+     * 获取文件修改时间，单位秒
+     * @param pathname
+     * @return
+     * @throws IOException
+     * @throws CantFindFileEx
+     */
+    public static long fileModifyTimeDuration(String pathname) throws IOException, CantFindFileEx {
+
+        Path path = Paths.get(pathname);
+        if (!Files.exists(path)) {
+            throw  new CantFindFileEx(pathname+" not exst");
+        }
+
+        BasicFileAttributes attrs = Files.readAttributes(path, BasicFileAttributes.class);
+        Instant createTime = attrs.lastModifiedTime().toInstant();
+        Instant now = Instant.now();
+        Duration duration = Duration.between(createTime, now);
+
+        return duration.toSeconds() ;
+
+    }
+
+
+
+    /**
+     * 读取文件创建时间，如果与当前时间相差超过5分钟，返回true
+     *
+     * @param pathname
+     * @param i
+     * @return
+     */
+    private static long FileCreateTimeGrtThan(String pathname) throws IOException, CantFindFileEx {
+
+        Path path = Paths.get(pathname);
+        if (!Files.exists(path)) {
+            throw  new CantFindFileEx(pathname+" not exst");
+        }
+
+        BasicFileAttributes attrs = Files.readAttributes(path, BasicFileAttributes.class);
+        Instant createTime = attrs.creationTime().toInstant();
+        Instant now = Instant.now();
+        Duration duration = Duration.between(createTime, now);
+
+        return duration.toMinutes() ;
+
+    }
+
 
     public static void delFile(String delfile) {
         new File(delfile).delete();
